70a8343d02f79e7939f237156f1e440a3eae8f27
Rafie <rafiul.faisal@gmail.com>
2023-12-09 17:44:17 +0600

    Formik

--- src/components/contract us/ContractUs.jsx
+++ src/components/contract us/ContractUs.jsx
@@ -1,52 +1,100 @@
 import React, { useState } from 'react'
 import './ContractUs.css'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { colors } from '@mui/material';
+
 function ContractUs() {
-    const [info, setinfo] = useState({ name: '', email: '', contract: '', address: '', subject: '', commnet: '' })
-    const allInfo=[];
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setinfo((prevInfo) => ({
-            ...prevInfo, [name]: value,
-        }));
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        allInfo.push(...allInfo, info)
-        console.log('Form data',info);
-        console.log('All info: ',allInfo);
-        setinfo(
-            { name: '', email: '', contract: '', address: '', subject: '', commnet: '' }
-        )
-    }
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            contract: '',
+            address: '',
+            subject: '',
+            comment: '',
+        },
+        validationSchema: Yup.object({
+            name: Yup.string().min(2, "Must be 2 character or more").max(15, "Must be 15 character or less").required("Required"),
+            email: Yup.string().email("Invalid email address").required("Required"),
+            contract: Yup.string().min(11, "Must be 11 character").max(11, "Must be 11 character").required("Required"),
+            address: Yup.string().min(2, "Must be 2 character or more").max(25, "Must be 15 character or less").required("Required"),
+            subject: Yup.string().min(6, "Must be 6 character or more").required(),
+            comment: Yup.string().max(200, "Must be 200 character or less")
+        }),
+        onSubmit: values => {
+            console.log("on submit clicked")
+            console.log(values);
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
     return (
         <div className='contractUs-container'>
             <div className="contractUs-heading">
                 <h1>Contract Us</h1>
                 <p>Red Grocer Limited is an online grocery supermarket available in Chattogram City Corporation area. Our goal is to provide fast service and good grocery products to the people of Chittagong in any condition to save their valuable time.</p>
             </div>
-            <div className="input-container">
+
+            <form onSubmit={formik.handleSubmit} className="input-container">
+
                 <div className="input-field">
-                    <input type='text' placeholder='Name' name='name' value={info.name} onChange={handleChange} required />
+
+                    <input className={`${formik.touched.name && formik.errors.name ? "invalid" : ""}`} type='text' placeholder='Name' name='name' id='name' {...formik.getFieldProps('name')} />
+                    {formik.touched.name && formik.errors.name ? (
+                        <div className='error-msg'>{formik.errors.name}</div>
+                    ) : null}
                 </div>
                 <div className="input-field">
-                    <input type='email' placeholder='Email' name='email' value={info.email} onChange={handleChange} required />
+
+                    <input type='email' placeholder='Email' name='email' id='email' {...formik.getFieldProps('email')} />
+                    {formik.touched.email && formik.errors.email ? (
+                        <div className='error-msg'>{formik.errors.email}</div>
+                    ) : null}
                 </div>
                 <div className="input-field">
-                    <input type='text' placeholder='Contract number' name='contract' value={info.contract} onChange={handleChange} required />
+
+                    <input type='text' placeholder='Contract number' name='contract' id='contract' {...formik.getFieldProps('contract')} />
+                    {formik.touched.contract && formik.errors.contract ? (
+                        <div className='error-msg'>{formik.errors.contract}</div>
+                    ) : null}
                 </div>
-                <div className="input-field">
-                    <input type='text' placeholder='Address' name='address' value={info.address} onChange={handleChange} required />
-                </div>
-                <div className="input-field">
-                    <input type='text' placeholder='Subject' name='subject' value={info.subject} onChange={handleChange} required />
-                </div>
-                <div className="input-field">
-                    <textarea placeholder='Your message' name='commnet' value={info.commnet} onChange={handleChange} />
+                <div className="input-field-container">
+                <div className="input-area">
+                    <input type='text' required />
+                    <div className="labeline">Name<div className='red'>*</div> </div>
                 </div>
             </div>
-            <button onClick={handleSubmit} type='submit'>Submit</button>
+                <div className="input-field">
+
+                    <input type='text' placeholder='Address' name='address' id='address' {...formik.getFieldProps('address')} />
+                    {formik.touched.address && formik.errors.address ? (
+                        <div className='error-msg'>{formik.errors.address}</div>
+                    ) : null}
+                </div>
+                <div className="input-field">
+
+                    <input type='text' placeholder='Subject' name='subject' id='subject' {...formik.getFieldProps('subject')} />
+                    {formik.touched.subject && formik.errors.subject ? (
+                        <div className='error-msg'>{formik.errors.subject}</div>
+                    ) : null}
+                </div>
+                <div className="input-field">
+
+                    <textarea placeholder='Your message' name='comment' id='comment' {...formik.getFieldProps('comment')} />
+                    {formik.touched.comment && formik.errors.comment ? (
+                        <div className='error-msg'>{formik.errors.comment}</div>
+                    ) : null}
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            {/* material input try */}
+            
         </div>
     )
 }
 
 export default ContractUs
+
+
+
+
